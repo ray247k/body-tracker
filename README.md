1fbbb7f46485d2d8f16697827c3a546e50caedbd
Ray <ray247k@gmail.com>
2020-03-31 23:06:48 +0800

    Add vue-chart.js
    - 加入 vue-chart.js 套件，資料尚未介接 API

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 
 也許加上一些事件紀錄的 tag
 
+使用 chart.js 來做圖表顯示的部分，安裝了 vue-chartjs 的 npm 套件
+
 ## Project setup
 ```
 npm install
---
+++ src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          "2019/11/19",
+          "2019/12/01",
+          "2019/12/29",
+          "2020/01/30",
+          "2020/03/03",
+        ],
+        datasets: [
+          {
+            label: "體脂肪率（％）",
+            data: [29.4, 28.1, 28.4, 30.3, 29.7],
+            backgroundColor: "transparent",
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            pointBackgroundColor: "rgba(1, 116, 188, 0.50)"
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: "體脂肪率曲線"
+        }
+      }
+    );
+  }
+};
+</script>
